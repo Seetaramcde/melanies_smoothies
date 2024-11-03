d09f9c6cce67cc0d441ad11cda138b97677db2d6
Seetaramcde <sramcde@gmail.com>
2024-11-03 22:00:33 +0000

    Update streamlit_app.py
    view API res in json fomat

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
