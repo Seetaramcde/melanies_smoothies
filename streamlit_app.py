971d00db65f67e10cd58a3de7c7d1891f8e67d1c
Seetaramcde <sramcde@gmail.com>
2024-11-03 22:27:51 +0000

    Update streamlit_app.py
    added pandas search_on logic

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.stop()
 
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 ingredients_list = st.multiselect('Choose up to 5 fruits!', my_dataframe, placeholder="Choose a Fruit!", max_selections=5)
 
@@ -37,6 +37,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '  
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + fruit_chosen + """' , '"""+ name_on_order + """')"""
         #st.write(my_insert_stmt)
